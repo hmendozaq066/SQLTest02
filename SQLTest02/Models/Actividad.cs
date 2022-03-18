aedd0a7ed46a20fa6a20de444c66190afeb9192c
HECTOR MENDOZA <hmendozaq@hotmail.com>
2022-03-18 17:56:00 -0600

    Agregar archivos de proyecto.

---
+++ SQLTest02/Models/Actividad.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SQLTest02.Models
+{
+    public class Actividad
+    {
+        [Key]
+        public int Actividad_id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Descripcion { get; set; }
+        [Required]
+        [StringLength(1024)]
+        public string Detalles { get; set; }
+        [Display(Name = "Actividad resulta")]
+        public bool Resuelto { get; set; }
+        [Required]
+        public string Estado_id { get; set; }
+
+        [ForeignKey("Estado_id")]
+        public Estado Estado { get; set; }
+    }
+}
